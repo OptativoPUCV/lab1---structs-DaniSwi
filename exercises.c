e97e1a1eeacbd76a4e9737c1f83d1d62f3ce9c21
Your Name <you@example.com>
2024-03-14 21:18:56 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -9,10 +9,10 @@ Ejercicio 1: Encuentra el Elemento Mayor
 Descripción: Escribe una función que tome un arreglo de enteros
 y su tamaño, y devuelva el valor más grande del arreglo.
 */
-int findMax(int arr[], int size){
+int findMax(int arr[], int size) {
   int max = arr[0];
-  for(int i=0;i<size;++i){
-    if(arr[i] > max)
+  for (int i = 0; i < size; ++i) {
+    if (arr[i] > max)
       max = arr[i];
   }
   return max;
@@ -23,15 +23,15 @@ Ejercicio 2: Invertir un Arreglo
 Descripción: Escribe una función que tome un arreglo y su tamaño, y luego
 invierta el orden de sus elementos.
 */
-void reverseArray(int arr[], int size){
+void reverseArray(int arr[], int size) {
   int temp;
   int k = 0;
-  for(int i=size-1;i>=k;--i){
+  for (int i = size - 1; i >= k; --i) {
     temp = arr[k];
     arr[k] = arr[i];
     arr[i] = temp;
     ++k;
-  }  
+  }
 }
 
 /*
@@ -40,12 +40,12 @@ Descripción: Escribe una función que tome un arreglo de enteros
 y su tamaño, y luego devuelva un nuevo arreglo que contenga solo
 los números pares del arreglo original.
 */
-int *filterEvenNumbers(int arr[], int size, int *newSize){
+int *filterEvenNumbers(int arr[], int size, int *newSize) {
   *(newSize) = 0;
   int *arrayNuevo;
   arrayNuevo = (int *)malloc(sizeof(int));
-  for(int i=0;i<size;++i){
-    if(arr[i] % 2 == 0){
+  for (int i = 0; i < size; ++i) {
+    if (arr[i] % 2 == 0) {
       arrayNuevo[*(newSize)] = arr[i];
       ++*(newSize);
     }
@@ -53,17 +53,17 @@ int *filterEvenNumbers(int arr[], int size, int *newSize){
   return arrayNuevo;
 }
 
-/* 
+/*
 Ejercicio 4: Fusión de dos Arreglos Ordenados
 Descripción: Escribe una función que tome dos arreglos
 ordenados y sus tamaños, y luego fusione estos dos
 arreglos en un tercer arreglo también ordenado.
-
+*/
 void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,
-                       int result[]){
+                       int result[]) {
   int i = 0, j = 0;
-  for(int k=0;k<size1+size2;++k){
-    if(arr1[i] <= arr2[j] && i < j){
+  for (int k = 0; k < size1 + size2; ++k) {
+    if (arr1[i] <= arr2[j] && i < size1) {
       result[k] = arr1[i];
       ++i;
     } else {
@@ -72,64 +72,57 @@ void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,
     }
   }
 
+  /*
+  Ejercicio 5: Comprobación de Ordenación
+  Descripción: Escribe una función que tome un arreglo y su tamaño,
+  y luego devuelva 1 si el arreglo está ordenado en orden ascendente,
+    0 si no está ordenado, y -1 si está ordenado en orden descendente.
+  */
+  int checkSorted(int arr[], int size) { for (int i = 0; i < size;) }
 
-Ejercicio 5: Comprobación de Ordenación
-Descripción: Escribe una función que tome un arreglo y su tamaño,
-y luego devuelva 1 si el arreglo está ordenado en orden ascendente,
-  0 si no está ordenado, y -1 si está ordenado en orden descendente.
+  /*
+  Ejercicio 6: Información de una Biblioteca
+  Descripción: Vamos a representar la información de una biblioteca. En la
+  biblioteca, hay libros, y cada libro tiene un autor. Queremos organizar esta
+  información utilizando estructuras anidadas en C. Escribe la función para
+  inicializar la información de un libro.
+  */
 
-int checkSorted(int arr[], int size){
-  for(int i=0;i<size;++i){
-    
+  typedef struct {
+    char nombre[50];
+    int anioNacimiento;
+  } Autor;
+
+  typedef struct {
+    char titulo[100];
+    Autor autor;
+    int anioPublicacion;
+  } Libro;
+
+  void inicializarLibro(Libro * libro, const char *titulo,
+                        const char *nombreAutor, int anioNacimiento,
+                        int anioPublicacion) {
+    libro = (Libro *)malloc(sizeof(Libro));
+    strcpy(libro->titulo, titulo);
+    strcpy(libro->autor->nombre, nombreAutor);
+    libro->autor->anioNacimiento = anioNacimiento;
+    libro->anioPublicacion = anioPublicacion;
   }
-}
 
+  /*
+  Ejercicio 7: Lista enlazada de números
+  Descripción: Escribe una función que tome un arreglo de enteros y su tamaño, y
+  luego cree una lista enlazada usando la estructura Nodo. Cada nodo de la lista
+  debe contener un número del arreglo, y los nodos deben estar enlazados en el
+  mismo orden que los números aparecen en el arreglo. El último nodo de la lista
+  debe apuntar a NULL. La función debe devolver un puntero al primer nodo de la
+  lista enlazada.
+  Recuerda reservar memoria dinámica para cada nodo usando malloc.
+    */
 
-Ejercicio 6: Información de una Biblioteca
-Descripción: Vamos a representar la información de una biblioteca. En la
-biblioteca, hay libros, y cada libro tiene un autor. Queremos organizar esta
-información utilizando estructuras anidadas en C. Escribe la función para
-inicializar la información de un libro.
+  typedef struct {
+    int numero;
+    struct nodo *siguiente; // puntero al siguiente nodo
+  } Nodo;
 
-
-typedef struct {
-  char nombre[50];
-  int anioNacimiento;
-} Autor;
-
-typedef struct {
-  char titulo[100];
-  Autor autor;
-  int anioPublicacion;
-} Libro;
-
-void inicializarLibro(Libro *libro, const char *titulo, const char *nombreAutor,
-                      int anioNacimiento, int anioPublicacion){
-  libro = (Libro *)malloc(sizeof(Libro));
-  strcpy(libro->titulo, titulo);
-  strcpy(libro->autor->nombre, nombreAutor);
-  libro->autor->anioNacimiento = anioNacimiento;
-  libro->anioPublicacion = anioPublicacion;
-}
-
-/*
-Ejercicio 7: Lista enlazada de números
-Descripción: Escribe una función que tome un arreglo de enteros y su tamaño, y
-luego cree una lista enlazada usando la estructura Nodo. Cada nodo de la lista
-debe contener un número del arreglo, y los nodos deben estar enlazados en el
-mismo orden que los números aparecen en el arreglo. El último nodo de la lista
-debe apuntar a NULL. La función debe devolver un puntero al primer nodo de la
-lista enlazada.
-Recuerda reservar memoria dinámica para cada nodo usando malloc.
-
-
-typedef struct{
-  int numero;
-  struct nodo *siguiente; // puntero al siguiente nodo
-} Nodo;
-
-Nodo *crearListaEnlazada(int arr[], int size) {
-  
-
-}
-*/
+  Nodo *crearListaEnlazada(int arr[], int size) {}
