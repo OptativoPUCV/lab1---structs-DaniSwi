1834e3c845c87c4fbf7d44ec24c7085b1e6786f0
Your Name <you@example.com>
2024-03-14 21:04:20 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -112,8 +112,8 @@ void inicializarLibro(Libro *libro, const char *titulo, const char *nombreAutor,
                       int anioNacimiento, int anioPublicacion){
   libro = (Libro *)malloc(sizeof(Libro));
   strcpy(libro->titulo, titulo);
-  strcpy(libro->autor.nombre, nombreAutor);
-  libro->autor.anioNacimiento = anioNacimiento;
+  strcpy(libro->autor->nombre, nombreAutor);
+  libro->autor->anioNacimiento = anioNacimiento;
   libro->anioPublicacion = anioPublicacion;
 }
 
