8f00a1d20c4bc7016bac5f3975b33380a40c7853
Your Name <you@example.com>
2024-03-15 01:12:03 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -79,20 +79,18 @@ Descripción: Escribe una función que tome un arreglo y su tamaño,
 y luego devuelva 1 si el arreglo está ordenado en orden ascendente,
   0 si no está ordenado, y -1 si está ordenado en orden descendente.
 */
-#include <stdbool.h>
-
 int checkSorted(int arr[], int size) {
-  bool ascendente = true;
-  bool descendiente = true;
+  int ascendente = 1;
+  int descendiente = 1;
   for(int i=0;i<size;++i){
     if(arr[i] > arr[i+1]){
-      descendiente = false;
+      descendiente = 0;
       break;
     }
   }
   for(int i=0;i<size;++i){
     if(arr[i] < arr[i+1]){
-      ascendente = false;
+      ascendente = 0;
       break;
     }
   }
